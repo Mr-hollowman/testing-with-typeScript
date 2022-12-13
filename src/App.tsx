cd2a0c7c89f9d4e48e20c0b3e656ba6f07fab882
mr-hollowman <selvamani825@gmail.com>
2022-12-13 16:19:31 +0530

    changes 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Application } from './components/greet/application/Application';
+import { Application } from './components/application/Application';
 
 function App() {
   return (
--- src/__test__/Application.test.tsx
+++ src/__test__/Application.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { Application } from "../components/greet/application/Application";
+import { Application } from "../components/application/Application";
 
 describe("application component", () => {
   test("render correctly", () => {
