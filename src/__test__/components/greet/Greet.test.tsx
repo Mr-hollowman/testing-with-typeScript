e5ff4462c4f85b6823e98c6792191619253581c6
mr-hollowman <selvamani825@gmail.com>
2022-12-13 16:28:38 +0530

    changes

--- src/__test__/components/greet/Greet.test.tsx
+++ src/__test__/components/greet/Greet.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react"
-import Greet from "../components/greet/Greet"
+import Greet from "../../../components/greet/Greet"
 
 describe("for the greet component",()=>{
     test("renders correctly",()=>{
